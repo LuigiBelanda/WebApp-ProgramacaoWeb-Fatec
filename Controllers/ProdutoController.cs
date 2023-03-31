fb5623b78f555c6bc265943fb9d579e2f4cb1878
LuigiBelanda <luigi.belanda.milani@gmail.com.br>
2023-03-31 20:35:34 -0300

    Criando a View Lista (Produto)

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp.Models;
 
+// Controller que irá cuidar das rotas relacionadas aos Produtos
 namespace WebApp.Controllers
 {
     public class ProdutoController : Controller
     {
+        // Criando a rota para listar os produtos cadastrados
         public IActionResult Lista()
         {
             return View();
         }
 
+        // Criando a rota para cadastrar os produtos
         public IActionResult Cadastrar()
         {
             return View();
