841ad46dd59a7f30602c0f2b0f22cf5e35fd4d5f
LuigiBelanda <luigi.belanda.milani@gmail.com.br>
2023-03-31 21:42:08 -0300

    Add novo Controller (Cliente), add Views Cliente (Cadastrar / Listar) e ajustando o menu global

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // Criando a rota para listar os usuários cadastrados
+        public IActionResult Lista()
+        {
+            return View();
+        }
+
+        // Criando a rota para cadastrar os usuários
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
