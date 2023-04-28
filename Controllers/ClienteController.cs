b44b3937b71596df1e7b989ff53ab395e260f692
LuigiBelanda <luigi.belanda.milani@gmail.com.br>
2023-04-28 20:14:53 -0300

    Model Clientes e Salvando Clientes

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,19 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class ClienteController : Controller
     {
+        public static List<ClientesModel> db = new List<ClientesModel>();
+
         // Criando a rota para listar os usuários cadastrados
         public IActionResult Lista()
         {
-            return View();
+            return View( db ); 
         }
 
         // Criando a rota para cadastrar os usuários
         public IActionResult Cadastrar()
         {
-            return View();
+            // Instanciando o nosso model de Clientes
+            ClientesModel model = new ClientesModel();
+
+            // Enviando junto com a tela o nosso model que foi instanciado
+            return View( model );
+        }
+
+        [HttpPost]
+        public IActionResult SalvarDados(ClientesModel cliente)
+        {
+            if (cliente.Id == 0)
+            {
+                Random rand = new Random();
+                cliente.Id = rand.Next(1, 9999);
+
+                db.Add(cliente);
+            }
+
+            return RedirectToAction("Lista");
         }
     }
 }
---
+++ Models/ClientesModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApp.Models
+{
+    public class ClientesModel
+    {
+        // get e set abstraídos
+        // { get; set; }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        
+        
+    }
+}
