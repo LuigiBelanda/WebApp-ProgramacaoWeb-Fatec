117fba213d551c9c55579e1456e77994dee0e239
LuigiBelanda <luigi.belanda.milani@gmail.com.br>
2023-04-28 21:46:20 -0300

    Editar e Excluir Produtos

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -66,7 +66,6 @@ namespace WebApp.Controllers
             {
                 return RedirectToAction("Lista");
             }
-
         }
     }
 }
--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -33,8 +33,39 @@ namespace WebApp.Controllers
 
                 db.Add(produto);
             }
+            else
+            {
+                int indice = db.FindIndex(a => a.Id == produto.Id);
+                db[indice] = produto;
+            }
 
             return RedirectToAction("Lista");
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            ProdutosModel item = db.Find(a => a.Id == id);
+
+            if (item != null)
+            {
+                db.Remove(item);
+            }
+
+            return RedirectToAction("Lista");
+        }
+
+        public IActionResult Editar(int id)
+        {
+            ProdutosModel item = db.Find(produto => produto.Id == id);
+
+            if (item != null)
+            {
+                return View(item);
+            }
+            else
+            {
+                return RedirectToAction("Lista");
+            }
         }
     }
 }
