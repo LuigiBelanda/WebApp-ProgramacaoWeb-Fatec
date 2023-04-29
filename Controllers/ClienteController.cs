398c1e0fdc3a2c26b2a332adcd6a19a23fe27485
LuigiBelanda <luigi.belanda.milani@gmail.com.br>
2023-04-28 21:33:21 -0300

    Editar e Excluir Clientes

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -33,8 +33,40 @@ namespace WebApp.Controllers
 
                 db.Add(cliente);
             }
+            else
+            {
+                int indice = db.FindIndex(a => a.Id == cliente.Id);
+                db[indice] = cliente;
+            }
 
             return RedirectToAction("Lista");
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            ClientesModel item = db.Find(a => a.Id == id);
+
+            if (item != null)
+            {
+                db.Remove(item);
+            }
+
+            return RedirectToAction("Lista");
+        }
+
+        public IActionResult Editar(int id) 
+        {
+            ClientesModel item = db.Find(cliente => cliente.Id == id);
+
+            if (item != null)
+            {
+                return View(item);
+            }
+            else
+            {
+                return RedirectToAction("Lista");
+            }
+
         }
     }
 }
